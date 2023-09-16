e7bafba99c402b7ff8f55936a5c4ddf180db374f
glimpse <1908045405@qq.com>
2023-09-16 14:18:35 +0800

    增加拦截器，打印接口耗时

---
+++ src/main/java/com/zm/config/SpringMvcConfig.java
@@ -0,0 +1,20 @@
+package com.zm.config;
+
+
+import com.zm.interceptor.LogInterceptor;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import javax.annotation.Resource;
+
+@Configuration
+public class SpringMvcConfig implements WebMvcConfigurer {
+
+    @Resource
+    LogInterceptor loginInterceptor;
+
+    public void addInterceptors(InterceptorRegistry registry){
+        registry.addInterceptor(loginInterceptor).addPathPatterns("/login");
+    }
+}
