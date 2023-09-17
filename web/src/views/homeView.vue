490a20b435f15aa7534ad449230bb495f6d0f399
glimpse <1908045405@qq.com>
2023-09-17 16:31:46 +0800

    默认显示欢迎页面，点击欢迎菜单时，可以显示欢迎页面

--- web/src/views/homeView.vue
+++ web/src/views/homeView.vue
@@ -6,22 +6,24 @@
           v-model:openKeys="openKeys"
           mode="inline"
           :style="{ height: '100%', borderRight: 0 }"
+          @click="handleClick"
       >
         <a-menu-item key="welcome">
-          <MailOutlined />
+          <MailOutlined/>
           <span>欢迎</span>
         </a-menu-item>
-        <a-sub-menu v-for="item in level1" :key="item.id" >
+        <a-sub-menu v-for="item in level1" :key="item.id">
           <template v-slot:title>
-            <span><user-outlined />{{item.name}}</span>
+            <span><user-outlined/>{{ item.name }}</span>
           </template>
           <a-menu-item v-for="child in item.children" :key="child.id">
-            <MailOutlined /><span>{{child.name}}</span>
+            <MailOutlined/>
+            <span>{{ child.name }}</span>
           </a-menu-item>
         </a-sub-menu>
-<!--        <a-menu-item key="tip" :disabled="true">-->
-<!--          <span>以上菜单在分类管理配置</span>-->
-<!--        </a-menu-item>-->
+        <!--        <a-menu-item key="tip" :disabled="true">-->
+        <!--          <span>以上菜单在分类管理配置</span>-->
+        <!--        </a-menu-item>-->
       </a-menu>
     </a-layout-sider>
     <a-layout style="padding: 0 24px 24px">
@@ -33,15 +35,20 @@
       <a-layout-content
           :style="{ background: '#fff', padding: '24px', margin: 0, minHeight: '280px' }"
       >
-<!--       :pagination="pagination" 分页 -->
-        <a-list item-layout="vertical" size="large" :grid="{ gutter: 20, column: 3}"  :data-source="ebooks">
-<!--          循环ebooks，读取数据-->
+        <!--        欢迎页面-->
+        <div class="welcome" v-show="isShowWelcome">
+          <the-welcome>欢迎来到电子书库</the-welcome>
+        </div>
+        <!--       :pagination="pagination" 分页 -->
+        <a-list v-show="!isShowWelcome" item-layout="vertical" size="large" :grid="{ gutter: 20, column: 3}"
+                :data-source="ebooks">
+          <!--          循环ebooks，读取数据-->
           <template #renderItem="{ item }">
             <a-list-item key="item.name">
               <template #actions>
-<!--                三个小图标-->
+                <!--                三个小图标-->
                 <span v-for="{ icon, text } in actions" :key="icon">
-                  <component :is="icon" style="margin-right: 6px" />
+                  <component :is="icon" style="margin-right: 6px"/>
                   {{ text }}
                 </span>
               </template>
@@ -49,7 +56,9 @@
                 <template #title>
                   <a :href="item.href">{{ item.name }}</a>
                 </template>
-                <template #avatar><a-avatar :src="item.cover" /></template>
+                <template #avatar>
+                  <a-avatar :src="item.cover"/>
+                </template>
               </a-list-item-meta>
             </a-list-item>
           </template>
@@ -61,7 +70,8 @@
 
 <script lang="ts">
 //content
-import { StarOutlined, LikeOutlined, MessageOutlined } from '@ant-design/icons-vue';
+import {StarOutlined, LikeOutlined, MessageOutlined} from '@ant-design/icons-vue';
+
 const listData: Record<string, string>[] = [];
 
 
@@ -74,23 +84,36 @@ const handleQueryCategory = () => {
   axios.get("/category/list").then((response) => {
     const data = response.data;
     //如果返回成功就进行查询 加了参数验证 如果page size异常就会报错
-    if (data.success){
+    if (data.success) {
       categorys = data.content;
-      console.log("原始数组",categorys);
+      console.log("原始数组", categorys);
 
       level1.value = []
-      level1.value = Tool.array2Tree(categorys,0);
-      console.log("树形结构",level1.value)
-    }else {
+      level1.value = Tool.array2Tree(categorys, 0);
+      console.log("树形结构", level1.value)
+    } else {
       //使用ant design vue的message
       message.error(data.message)
     }
   });
 
-  const handleClick =() => {
-    console.log("menu click")
-  };
 }
+//响应式变量 确认是否显示欢迎页面
+const isShowWelcome = ref(true);
+
+const handleClick = (value: any) => {
+  /*
+    if(value.key === 'welcome'){
+    isShowWelcome.value =true;
+  }else {
+    isShowWelcome.value =false;
+  }
+   */
+  isShowWelcome.value = value.key === 'welcome';
+}
+
+
+
 
 const pagination = {
   onChange: (page: number) => {
@@ -99,39 +122,44 @@ const pagination = {
   pageSize: 3,
 };
 const actions: Record<string, any>[] = [
-  { icon: StarOutlined, text: '156' },
-  { icon: LikeOutlined, text: '156' },
-  { icon: MessageOutlined, text: '2' },
+  {icon: StarOutlined, text: '156'},
+  {icon: LikeOutlined, text: '156'},
+  {icon: MessageOutlined, text: '2'},
 ];
 
-import { defineComponent,onMounted,ref} from 'vue';
+import {defineComponent, onMounted, ref} from 'vue';
 import axios from "axios";
 import {Tool} from "@/util/tool";
 import {message} from "ant-design-vue";
+
 export default defineComponent({
   name: 'HomeView',
-  setup(){
+  setup() {
     const ebooks = ref();
 
     //初始化
-    onMounted(() =>{
+    onMounted(() => {
       handleQueryCategory();
       axios.get("/list").then((response) => {
-       const data = response.data;
-       ebooks.value = data.content;
+        const data = response.data;
+        ebooks.value = data.content;
       });
     })
 
-    return{
+    return {
       ebooks,
       actions: [
-        { icon: StarOutlined, text: '156' },
-        { icon: LikeOutlined, text: '156' },
-        { icon: MessageOutlined, text: '2' },
+        {icon: StarOutlined, text: '156'},
+        {icon: LikeOutlined, text: '156'},
+        {icon: MessageOutlined, text: '2'},
       ],
 
       //树级菜单
       level1,
+
+      // 欢迎界面
+      isShowWelcome,
+      handleClick,
     }
   }
 });
@@ -140,11 +168,11 @@ export default defineComponent({
 </script>
 
 <style scoped>
-  .ant-avatar{
-    width: 50px;
-    height: 50px;
-    line-height: 50px;
-    border-radius: 8%;
-    margin: 5px 0;
-  }
+.ant-avatar {
+  width: 50px;
+  height: 50px;
+  line-height: 50px;
+  border-radius: 8%;
+  margin: 5px 0;
+}
 </style>
