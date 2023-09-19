d05fb0b555d2e41aae5582ece7b254927f6431ae
glimpse <1908045405@qq.com>
2023-09-19 10:30:56 +0800

    让表格树默认展开

--- web/src/views/admin/admin-doc.vue
+++ web/src/views/admin/admin-doc.vue
@@ -25,12 +25,14 @@
               </a-form>
             </p>
             <a-table
+                v-if="level1.length > 0"
                 :columns="columns"
                 :row-key="record => record.id"
                 :data-source="level1"
                 :loading="loading"
                 :pagination="false"
                 size="small"
+                :defaultExpandAllRows="true"
             >
               <template #cover="{ text: cover }">
                 <img v-if="cover" :src="cover" alt="avatar" />
@@ -174,7 +176,7 @@ export default defineComponent({
      */
     //一级文档树，children属性就是二级文档
     const level1 = ref();
-
+    level1.value = [];
     /**
      * 数据查询
      **/
