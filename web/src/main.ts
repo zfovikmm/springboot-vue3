19e9c0fade009c7e63cf3f533ad9937d22f617cc
glimpse <1908045405@qq.com>
2023-09-22 10:21:42 +0800

    集成WebSocket

--- web/src/main.ts
+++ web/src/main.ts
@@ -44,8 +44,6 @@ axios.interceptors.response.use(function (response) {
     return Promise.reject(error);
 });
 
-
-
 const app = createApp(App)
 app.use(store).use(router).use(Antd).mount('#app')
 
